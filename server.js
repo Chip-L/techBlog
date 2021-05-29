9d71e3b22b48fa3d19fdd2fd12ce901b776b8752
Chip Long <chip_long@yahoo.com>
2021-05-28 22:13:45 -0600

    added links into server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // User.sync({ alter: true }) - This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
 sequelize
-  .sync({ alter: true })
+  .sync({ force: true })
   .then(
     app.listen(PORT, () =>
       console.log(`Tech Blog is now listening to PORT: ${PORT}`)
