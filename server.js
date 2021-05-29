cec0f7f779a0a5c3f7cd98124ea29ff8211c314f
Chip Long <chip_long@yahoo.com>
2021-05-28 21:30:59 -0600

    changed the way sequelize syncs to alter

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// User.sync({ alter: true }) - This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
 sequelize
-  .sync({ force: false })
+  .sync({ alter: true })
   .then(
     app.listen(PORT, () =>
       console.log(`Tech Blog is now listening to PORT: ${PORT}`)
