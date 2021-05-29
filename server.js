ef5f93895c00333704385f8d9c4291adb751283d
Chip Long <chip_long@yahoo.com>
2021-05-28 22:14:37 -0600

    updated server and main.handelbars

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+// bring in modules needed
 const express = require("express");
-const sequelize = require("./config/connection");
+const path = require("path");
 
+// configure modules
+const sequelize = require("./config/connection");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+
+// setup server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// browser io middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// routing middleware
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
 // User.sync({ alter: true }) - This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
 sequelize
