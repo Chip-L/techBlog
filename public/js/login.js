b6d50eca068561f6169d668bac45773b2e562c1c
Chip Long <chip_long@yahoo.com>
2021-05-31 15:03:29 -0600

    added login capability

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+
+  console.log(username, password);
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ name: username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in");
+    }
+  }
+}
+
+document
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
