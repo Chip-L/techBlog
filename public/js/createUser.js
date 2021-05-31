5dc88b490900d90f8843bd5a4bc1e4c5dcda693e
Chip Long <chip_long@yahoo.com>
2021-05-31 15:32:15 -0600

    added create user form

---
+++ public/js/createUser.js
@@ -0,0 +1,26 @@
+async function createFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+
+  console.log(username, password);
+
+  if (username && password) {
+    const response = await fetch("/api/users/", {
+      method: "POST",
+      body: JSON.stringify({ name: username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in");
+    }
+  }
+}
+
+document
+  .querySelector("#sign-up-form")
+  .addEventListener("submit", createFormHandler);
